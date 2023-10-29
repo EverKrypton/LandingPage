e5fe53719c772714027a168f85ad3102ea8f7b4d
EverKrypton <88674669+EverKrypton@users.noreply.github.com>
2023-10-29 01:15:11 -0400

    Create SocialsSection.js

---
+++ SocialsSection.js
@@ -0,0 +1,15 @@
+// SocialsSection.js
+import React from 'react';
+
+class SocialsSection extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Socials</h1>
+        <p>This is the Socials section.</p>
+      </div>
+    );
+  }
+}
+
+export default SocialsSection;
