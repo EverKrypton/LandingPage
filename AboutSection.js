817f4ce717cfa00c2beb989d9b1454a6dafcceb4
EverKrypton <88674669+EverKrypton@users.noreply.github.com>
2023-10-29 01:13:07 -0400

    Create AboutSection.js

---
+++ AboutSection.js
@@ -0,0 +1,15 @@
+// AboutSection.js
+import React from 'react';
+
+class AboutSection extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>This is the About section.</p>
+      </div>
+    );
+  }
+}
+
+export default AboutSection;
