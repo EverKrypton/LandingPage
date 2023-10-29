c7a6456dac820db9a602bb9d812429d4c551ae46
EverKrypton <88674669+EverKrypton@users.noreply.github.com>
2023-10-29 01:14:28 -0400

    Create RoadmapSection.js

---
+++ RoadmapSection.js
@@ -0,0 +1,15 @@
+// RoadmapSection.js
+import React from 'react';
+
+class RoadmapSection extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Roadmap</h1>
+        <p>This is the Roadmap section.</p>
+      </div>
+    );
+  }
+}
+
+export default RoadmapSection;
